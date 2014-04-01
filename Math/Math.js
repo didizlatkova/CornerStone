36680a65cc1838e4f409e121975ef344cfe0b6a5
Dimitrina Zlatkova <didi.zlatkova93@gmail.com>
2014-04-01 13:24:47 +0300

    fixed corner case in line draw

--- Math/Math.js
+++ Math/Math.js
@@ -6,8 +6,18 @@ CornerStone.Math = function () {
 
 CornerStone.Math.prototype = function () {
     var getPointsForLine = function (x1, y1, x2, y2, n) {
-    	if (x1 == x2) {
-    		return;
+        if (x1 == x2) {
+            if (y1==y2) {
+                return;
+            }
+            else {
+                var result = [];
+                var interval = (Math.max(y1, y2) - Math.min(y1, y2)) / n;
+                for (var i = Math.min(y1, y2) ; i <= Math.max(y1, y2) ; i += interval) {
+                    y = i;
+                    result.push([x1, y]);
+                }
+            }
     	} else {
         var slope = (y2 - y1) / (x2 - x1);
         var b = y1 - slope * x1;
