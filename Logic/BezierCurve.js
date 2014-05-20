2e183bdf995787c8caf4091c446bf26bac89ac28
ngyurova <ngyurova@amplify.com>
2014-05-20 16:00:39 +0300

    Working drawing of curves (not really)

--- Logic/BezierCurve.js
+++ Logic/BezierCurve.js
@@ -18,47 +18,26 @@ CornerStone.BezierCurve.prototype = function () {
         point = new CornerStone.Point(),
         clickCount = 0,
         dragData = new Array();
-
-    drawTriangle = function (ctx) {
-        if (dragData[0] == undefined) {
-            dragData.push([this.first.x, this.first.y]);
-            dragData.push([this.second.x, this.second.y]);
-            dragData.push([this.third.x, this.third.y]);
-        }
-
-        var points = [];
-        points = points.concat(line.draw(ctx, dragData[0][0], dragData[0][1], dragData[1][0], dragData[1][1]));
-        points = points.concat(line.draw(ctx, dragData[1][0], dragData[1][1], dragData[2][0], dragData[2][1]));
-        points = points.concat(line.draw(ctx, dragData[0][0], dragData[0][1], dragData[2][0], dragData[2][1]));
-
-        dragData = new Array();
-        return points;
-    },
     
-    drawCasteljau = function (anchorPoints) {
-        tmp = new CornerStone.Point();
+    drawCasteljau = function (ctx, anchors) {
+        var points = [];
         for (var t = 0; t <= 1; t += 0.001) { 
-            tmp = getCasteljauPoint(points.length-1, 0, t);
-            //image.SetPixel(tmp.x, tmp.y, color);
+            var tempPoint = getCasteljauPoint(anchors.length-1, 0, t, anchors);
+            //console.log(tempPoint);
+            points = points.concat(tempPoint);
+            ctx.fillRect(tempPoint.x, tempPoint.y, 1, 1);
         }
-
-        getCasteljauPoint = function(r, i, t) { 
-            if(r == 0) return points[i];
-
-            var p1 = getCasteljauPoint(r - 1, i, t),
-                p2 = getCasteljauPoint(r - 1, i + 1, t);
-
-            return new  CornerStone.Point((int) ((1 - t) * p1.X + t * p2.X), (int) ((1 - t) * p1.Y + t * p2.Y));
-        }
+        return points;    
     }
 
+    getCasteljauPoint = function(r, i, t, anchors) { 
+            if(r == 0) return anchors[i];
 
-    
-    drawTempTriangle = function (ctx, x, y) {
-        line.draw(ctx, dragData[0][0], dragData[0][1], dragData[1][0], dragData[1][1]);
-        line.draw(ctx, dragData[1][0], dragData[1][1], x, y);
-        line.draw(ctx, dragData[0][0], dragData[0][1], x, y);
-    },
+            var p1 = getCasteljauPoint(r - 1, i, t, anchors),
+                p2 = getCasteljauPoint(r - 1, i + 1, t, anchors);
+
+            return new CornerStone.Point(Math.round((1 - t) * p1.x + t * p2.x), Math.round((1 - t) * p1.y + t * p2.y));
+        }
 
     click = function (ev) {
         ev = ev || event;
@@ -69,15 +48,18 @@ CornerStone.BezierCurve.prototype = function () {
             var a = new CornerStone.Point(dragData[0][0], dragData[0][1]),
                 b = new CornerStone.Point(dragData[1][0], dragData[1][1]),
                 c = new CornerStone.Point(dragData[2][0], dragData[2][1]),
-                    points = drawTriangle(CornerStone.context);
+                anchors = [a,b,c],
+                points = drawCasteljau(CornerStone.context, anchors);
 
             definingPoints.push(a);
             definingPoints.push(b);
             definingPoints.push(c);
-            elements.triangles.push(new CornerStone.Triangle(a, b, c, points));
+            
             this.first = a;
             this.second = b;
             this.third = c;
+
+            elements.curves.push(new CornerStone.BezierCurve(a, b, c, points));
 
             clickCount = 0;
             CornerStone.tempContext.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -91,13 +73,14 @@ CornerStone.BezierCurve.prototype = function () {
             line.draw(CornerStone.tempContext, dragData[0][0], dragData[0][1], ev.clientX, ev.clientY);
         } else if (clickCount == 2) {
             CornerStone.tempContext.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-            drawTempTriangle(CornerStone.tempContext, ev.clientX, ev.clientY);
+            line.draw(CornerStone.tempContext, dragData[0][0], dragData[0][1], dragData[1][0], dragData[1][1]);
+            line.draw(CornerStone.tempContext, dragData[1][0], dragData[1][1], ev.clientX, ev.clientY);
         };
     };
 
     return {
         click: click,
         move: move,
-        draw: drawTriangle
+        draw: drawCasteljau
     };
 }();
